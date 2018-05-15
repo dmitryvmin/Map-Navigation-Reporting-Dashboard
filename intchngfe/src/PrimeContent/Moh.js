3093ca431e0396224d8116e882efeda367040ab2
Dmitry <dmitrymin@gmail.com>
2018-05-15 11:33:17 -0700

    GG-123

--- intchngfe/src/PrimeContent/Moh.js
+++ intchngfe/src/PrimeContent/Moh.js
@@ -124,7 +124,7 @@ export default class Moh extends Component {
         that.mapPropsToTableColumns(json);
       }
     };
-    xhttp.open("GET", "http://20.36.19.106:9003/sensor", true);
+    xhttp.open("GET", "http://20.36.19.106:9000/sensor", true);
     xhttp.setRequestHeader('Authorization','Basic Z2xvYmFsLmdvb2Q6fkYoRzNtKUtQeT8/ZHd4fg==');
     xhttp.send();
   }
@@ -178,7 +178,6 @@ export default class Moh extends Component {
     mapPropsToTableColumns = (json) => {
       let device_info: any = [];
       json && json.sensors && json.sensors.map((d: any) => {
-      console.log('### d ###', d);
         let obj: any = {}
 
         // Sensor Info
