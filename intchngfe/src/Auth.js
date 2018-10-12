efe5c0d84a191a5f53e14b35edf62fe759dcc637
Dmitry <dmitrymin@gmail.com>
2018-10-12 14:56:18 -0700

    bugs

--- intchngfe/src/Auth.js
+++ intchngfe/src/Auth.js
@@ -1,31 +1,26 @@
+import * as React from 'react';
 import GGConsts from './Constants';
 
-class Auth = {
-  isAuthenticated: false,
-
+class Auth {
   authenticate(username, password,cb) {
-
-  	const creds: any = `${username}:${password}`;
-  	const header: any = {
+    const creds: any = `${username}:${password}`;
+    const header: any = {
         'Accept': 'application/json',
         'Authorization': 'Basic ' + btoa(creds)
     };
-
-    let context: any = this; 
-  	fetch(`${GGConsts.API}:${GGConsts.ADMIN_PORT}/account`, { headers: new Headers (header)})
+    fetch(`${GGConsts.API}:${GGConsts.ADMIN_PORT}/account`, { headers: new Headers (header)})
         .then(function(response) {
-			
-			if (response.status === 200) {
-				context.isAuthenticated = true
-				setTimeout(cb, 100)
-			} else {
-				console.warn('Wrong login creds ', creds);
-			}
+      
+      if (response.status === 200) {
+        setTimeout(() => {cb(true)}, 100)
+      } else {
+        console.warn('Wrong login creds ', creds);
+      }
 
-		}).catch(function(err) {
-			console.warn('Auth error', err)
-		});
-  },
+    }).catch(function(err) {
+      console.warn('Auth error', err)
+    });
+  }
 
   signout(cb) {
     this.isAuthenticated = false
--- intchngfe/src/Login.js
+++ intchngfe/src/Login.js
@@ -6,10 +6,38 @@ import * as React from 'react';
 //   Redirect,
 //   withRouter
 // } from 'react-router-dom';
+import GGConsts from './Constants';
+
 import TextField from 'material-ui-next/TextField';
 import Button from 'material-ui-next/Button';
 import Card from 'material-ui-next/Card';
-import Auth from 'Auth';
+
+const Auth = {
+  authenticate(username, password,cb) {
+    const creds: any = `${username}:${password}`;
+    const header: any = {
+        'Accept': 'application/json',
+        'Authorization': 'Basic ' + btoa(creds)
+    };
+    fetch(`${GGConsts.API}:${GGConsts.ADMIN_PORT}/account`, { headers: new Headers (header)})
+        .then(function(response) {
+      
+      if (response.status === 200) {
+        setTimeout(() => {cb(true)}, 100)
+      } else {
+        console.warn('Wrong login creds ', creds);
+      }
+
+    }).catch(function(err) {
+      console.warn('Auth error', err)
+    });
+  },
+
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100)
+  }
+}
 
 export default class Login extends React.Component {
   constructor (props, context) {
@@ -35,9 +63,9 @@ export default class Login extends React.Component {
     });
   };
 
-  keyPress = (e) => {
-      if(e.keyCode == 13) {
-        Auth.authenticate(this.state.username, this.state.password, this.authCallback());
+   keyPress = (e) => {
+      if(e.keyCode == 13){
+         Auth.authenticate(this.state.username, this.state.password, this.authCallback());
       }
    }
 
@@ -51,30 +79,30 @@ export default class Login extends React.Component {
 
     return (
       <Card style={{maxWidth: '500px', padding: '30px', marginTop: '30px', margin: '50px auto 0'}}>
-      		<p style={{textAlign: 'center', marginBottom: '2em'}}>Please login to view the Reporting Tool</p>
-	      	<form style={{display: 'flex',  justifyContent: 'center'}} noValidate autoComplete="off">
-				<TextField
-					id="username"
-					label="Username"
-					margin="normal"
-					style={{margin: '10px'}}
-					value={this.state.username}
-					onChange={this.handleChange('username')}
+          <p style={{textAlign: 'center', marginBottom: '2em'}}>Please login to view the Reporting Tool</p>
+          <form style={{display: 'flex',  justifyContent: 'center'}} noValidate autoComplete="off">
+        <TextField
+          id="username"
+          label="Username"
+          margin="normal"
+          style={{margin: '10px'}}
+          value={this.state.username}
+          onChange={this.handleChange('username')}
           onKeyDown={this.keyPress}
-					margin="normal"/>
-				<TextField
-					id="password"
-					label="Password"
-					type="password"
-					margin="normal"
-					style={{margin: '10px'}}
-					autoComplete="current-password"
-					value={this.state.password}
-					onChange={this.handleChange('password')}
+          margin="normal"/>
+        <TextField
+          id="password"
+          label="Password"
+          type="password"
+          margin="normal"
+          style={{margin: '10px'}}
+          autoComplete="current-password"
+          value={this.state.password}
+          onChange={this.handleChange('password')}
           onKeyDown={this.keyPress}
-					margin="normal" />
-			</form>
-			<button style={{backgroundColor: '#7cd33b',padding: '8px 16px', margin: '0 auto', display: 'inherit', marginTop: '20px', color: 'white'}} onClick={this.login}>Log in</button>
+          margin="normal" />
+      </form>
+      <button style={{backgroundColor: '#7cd33b',padding: '8px 16px', margin: '0 auto', display: 'inherit', marginTop: '20px', color: 'white'}} onClick={this.login}>Log in</button>
       </Card>
     )
   }
